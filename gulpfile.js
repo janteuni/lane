a8ccdfe92c4509b1ad0bed3f2ea02d15e1134f29
janteuni <janteuni@student.42.fr>
2015-05-18 18:22:20 +0200

    feat(build)

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,33 @@ var gulp = require('gulp');
 var server = require('gulp-server-livereload');
 var plumber = require('gulp-plumber');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var minifyCss = require('gulp-minify-css');
 
 gulp.task('sass', function() {
 	return gulp.src('app/style.scss')
 		.pipe(plumber())
 		.pipe(sass())
 		.pipe(gulp.dest('app/'));
+});
+
+gulp.task('build-css', function() {
+    return gulp.src('app/*.css')
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(minifyCss({compatibility: 'ie8'}))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('build', ['build-css'], function() {
+    return gulp.src([
+        'app/fonts/*',
+        'app/images/*',
+        'app/index.html'
+    ], { base: 'app' })
+        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('watch', function() {
